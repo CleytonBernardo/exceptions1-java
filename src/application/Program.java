16d582145e64e62ad82dd05917da16227ff2f8d8
Cleyton Bernardo <cleyton.bernardo@gmail.com>
2022-09-28 12:30:53 -0300

    solução menos ruim

--- src/application/Program.java
+++ src/application/Program.java
@@ -28,26 +28,16 @@ public class Program {
 			Reservation reservation = new Reservation(number, checkIn, checkOut);
 			System.out.println("Reservation: " + reservation);
 		
-		
 			System.out.print("\nEnter data to update the reservation: ");
 			System.out.print("Check-in date (dd/MM/yyyy): ");
 			checkIn = sdf.parse(sc.next());
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 			
-			Date now = new Date();
+			String error reservation.updateDates(checkIn, checkOut);
 			
-			if(checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for updates must be future dates");
-			}
-			else if(!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Check-ou date must be after check-in date");
-			}
-			else {
-				reservation = new Reservation(number, checkIn, checkOut);
-				System.out.println("Reservation: " + reservation);
-				
-			}
+			reservation = new Reservation(number, checkIn, checkOut);
+			System.out.println("Reservation: " + reservation);
 				
 		}
 		
